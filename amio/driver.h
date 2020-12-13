f3c8be6f9a5983d3ce7374fa256f6cc32fa86bfa
Michał Szymański <smiszym@gmail.com>
2020-12-13 23:02:14 +0100

    Unify variable naming

--- amio/driver.h
+++ amio/driver.h
@@ -7,10 +7,10 @@ struct Driver
 {
     void * (*create_state_object)(
         const char *client_name, struct Interface *interface);
-    void (*init)(void *state);
-    void (*destroy)(void *state);
-    void (*set_position)(void *handle, int position);
-    void (*set_is_transport_rolling)(void *handle, bool value);
+    void (*init)(void *driver_state);
+    void (*destroy)(void *driver_state);
+    void (*set_position)(void *driver_state, int position);
+    void (*set_is_transport_rolling)(void *driver_state, bool value);
 };
 
 #endif
--- amio/jack_driver.c
+++ amio/jack_driver.c
@@ -31,7 +31,7 @@ struct JackDriverState
     int frame_in_playspec;  /* position in the whole playspec */
 };
 
-static void jack_iface_init(void *state);
+static void jack_iface_init(void *driver_state);
 
 static void * jack_create_state_object(
     const char *client_name, struct Interface *interface)
@@ -54,25 +54,25 @@ static void * jack_create_state_object(
     return state;
 }
 
-static void jack_destroy(void *state)
+static void jack_destroy(void *driver_state)
 {
-    struct JackDriverState *jack_interface = state;
-    jack_client_close(jack_interface->client);
-    free(jack_interface);
+    struct JackDriverState *state = driver_state;
+    jack_client_close(state->client);
+    free(state);
 }
 
-static void jack_set_position(void *handle, int position)
+static void jack_set_position(void *driver_state, int position)
 {
     /* Runs on the I/O thread */
-    struct JackDriverState *client = handle;
-    client->frame_in_playspec = position;
+    struct JackDriverState *state = driver_state;
+    state->frame_in_playspec = position;
 }
 
-static void jack_set_is_transport_rolling(void *handle, bool value)
+static void jack_set_is_transport_rolling(void *driver_state, bool value)
 {
     /* Runs on the I/O thread */
-    struct JackDriverState *client = handle;
-    client->is_transport_rolling = value;
+    struct JackDriverState *state = driver_state;
+    state->is_transport_rolling = value;
 }
 
 static int process(jack_nframes_t nframes, void *arg)
@@ -123,22 +123,22 @@ static void jack_shutdown(void *arg)
     exit(1);
 }
 
-static void jack_iface_init(void *state)
+static void jack_iface_init(void *driver_state)
 {
     /* Runs on the Python thread */
 
-    struct JackDriverState *jack_interface = state;
+    struct JackDriverState *state = driver_state;
 
     const char **ports;
     jack_options_t options = JackNullOption;
     jack_status_t status;
 
-    jack_interface->is_transport_rolling = false;
-    jack_interface->frame_in_playspec = 0;
+    state->is_transport_rolling = false;
+    state->frame_in_playspec = 0;
 
-    jack_interface->client = jack_client_open(
-        jack_interface->client_name, options, &status, NULL);
-    if (jack_interface->client == NULL) {
+    state->client = jack_client_open(
+        state->client_name, options, &status, NULL);
+    if (state->client == NULL) {
         fprintf(stderr, "jack_client_open() failed, "
             "status = 0x%2.0x\n", status);
         if (status & JackServerFailed) {
@@ -150,71 +150,71 @@ static void jack_iface_init(void *state)
         fprintf(stderr, "JACK server started\n");
     }
     if (status & JackNameNotUnique) {
-        jack_interface->client_name =
-            jack_get_client_name(jack_interface->client);
+        state->client_name =
+            jack_get_client_name(state->client);
         fprintf(stderr, "Unique name `%s' assigned\n",
-            jack_interface->client_name);
+            state->client_name);
     }
 
     post_task_with_int_to_py_thread(
-        jack_interface->interface, py_thread_receive_frame_rate,
-        jack_get_sample_rate(jack_interface->client));
+        state->interface, py_thread_receive_frame_rate,
+        jack_get_sample_rate(state->client));
 
     jack_set_process_callback(
-        jack_interface->client, process, jack_interface);
-    jack_on_shutdown(jack_interface->client, jack_shutdown, 0);
+        state->client, process, state);
+    jack_on_shutdown(state->client, jack_shutdown, 0);
 
-    jack_interface->input_port_l = jack_port_register(
-        jack_interface->client, "input_l",
+    state->input_port_l = jack_port_register(
+        state->client, "input_l",
         JACK_DEFAULT_AUDIO_TYPE,
         JackPortIsInput, 0);
 
-    jack_interface->input_port_r = jack_port_register(
-        jack_interface->client, "input_r",
+    state->input_port_r = jack_port_register(
+        state->client, "input_r",
         JACK_DEFAULT_AUDIO_TYPE,
         JackPortIsInput, 0);
 
-    if ((jack_interface->input_port_l == NULL)
-            || (jack_interface->input_port_r == NULL)) {
+    if ((state->input_port_l == NULL)
+            || (state->input_port_r == NULL)) {
         fprintf(stderr, "No more JACK ports available\n");
         exit(1);
     }
 
-    jack_interface->output_port_l = jack_port_register(
-        jack_interface->client, "output_l",
+    state->output_port_l = jack_port_register(
+        state->client, "output_l",
         JACK_DEFAULT_AUDIO_TYPE,
         JackPortIsOutput, 0);
 
-    jack_interface->output_port_r = jack_port_register(
-        jack_interface->client, "output_r",
+    state->output_port_r = jack_port_register(
+        state->client, "output_r",
         JACK_DEFAULT_AUDIO_TYPE,
         JackPortIsOutput, 0);
 
-    if ((jack_interface->output_port_l == NULL)
-            || (jack_interface->output_port_r == NULL)) {
+    if ((state->output_port_l == NULL)
+            || (state->output_port_r == NULL)) {
         fprintf(stderr, "No more JACK ports available\n");
         exit(1);
     }
 
-    if (jack_activate(jack_interface->client)) {
+    if (jack_activate(state->client)) {
         fprintf(stderr, "Cannot activate JACK client");
         exit(1);
     }
 
-    ports = jack_get_ports(jack_interface->client, NULL, NULL,
+    ports = jack_get_ports(state->client, NULL, NULL,
                            JackPortIsPhysical|JackPortIsOutput);
     if (ports == NULL) {
         fprintf(stderr, "No physical capture ports\n");
         exit(1);
     }
 
-    if (jack_connect(jack_interface->client,
-            ports[0], jack_port_name(jack_interface->input_port_l))) {
+    if (jack_connect(state->client,
+            ports[0], jack_port_name(state->input_port_l))) {
         fprintf(stderr, "Cannot connect input ports\n");
     }
 
-    if (jack_connect(jack_interface->client,
-            ports[1], jack_port_name(jack_interface->input_port_r))) {
+    if (jack_connect(state->client,
+            ports[1], jack_port_name(state->input_port_r))) {
         fprintf(stderr, "Cannot connect input ports\n");
     }
 
@@ -224,26 +224,26 @@ static void jack_iface_init(void *state)
      */
     jack_latency_range_t capture_latency;
     jack_port_get_latency_range(
-        jack_port_by_name(jack_interface->client, ports[0]),
+        jack_port_by_name(state->client, ports[0]),
         JackCaptureLatency,
         &capture_latency);
 
     jack_free(ports);
 
-    ports = jack_get_ports(jack_interface->client, NULL, NULL,
+    ports = jack_get_ports(state->client, NULL, NULL,
                            JackPortIsPhysical|JackPortIsInput);
     if (ports == NULL) {
         fprintf(stderr, "No physical playback ports\n");
         exit(1);
     }
 
-    if (jack_connect(jack_interface->client,
-            jack_port_name(jack_interface->output_port_l), ports[0])) {
+    if (jack_connect(state->client,
+            jack_port_name(state->output_port_l), ports[0])) {
         fprintf(stderr, "Cannot connect output ports\n");
     }
 
-    if (jack_connect(jack_interface->client,
-            jack_port_name(jack_interface->output_port_r), ports[1])) {
+    if (jack_connect(state->client,
+            jack_port_name(state->output_port_r), ports[1])) {
         fprintf(stderr, "Cannot connect output ports\n");
     }
 
@@ -253,14 +253,14 @@ static void jack_iface_init(void *state)
      */
     jack_latency_range_t playback_latency;
     jack_port_get_latency_range(
-        jack_port_by_name(jack_interface->client, ports[0]),
+        jack_port_by_name(state->client, ports[0]),
         JackPlaybackLatency,
         &playback_latency);
 
     jack_free(ports);
 
     /* We should handle situations where min!=max, but these are rare */
-    jack_interface->total_latency = capture_latency.min + playback_latency.min;
+    state->total_latency = capture_latency.min + playback_latency.min;
 }
 
 struct Driver jack_driver = {
